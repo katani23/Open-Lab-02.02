77b8cf5e4df7581646d7734ba333746c0a41b02f
adamp_000 <adamp_000@DAVID-PC>
2020-11-19 19:56:15 +0100

    2.02

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -6,7 +6,12 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+            string output = "";
+            for (int i = 0; i < numbers.Lenght; i++)
+            {
+                output = output + numbers[i].ToString("F", CultureInfo.InvariantCulture) + " ";
+            }
+            Console.Write(output.Substring(0, output.Legth - 1));
         }
     }
 }
